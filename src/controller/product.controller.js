09b710118ed1501e1293840666567e341c2bf9e1
AayushParashar28 <ayushparashar6203@gmail.com>
2024-06-14 16:42:41 +0530

    Product created

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,2 +1,23 @@
 const ProductModel = require("../Model/product.model");
 
+exports.createProduct = async (req , res) => {
+    try {
+        const Product = {
+            ProductName:req.body.ProductName,
+            Category:req.body.Category,
+            Price:req.body.price,
+            Id:req.body.Id
+        }
+
+        const result = await ProductModel.create(Product);
+        return res.status(201).json({
+            sucess: true,
+            response: result
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: "Internal Server Error"
+        });
+    }
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,3 +1,9 @@
 const express =  require("express")
+const ProductController = require("../controller/product.controller")
 
-const ProductRoute = require("../controller/product.controller")
+const ProductRouter = express.Router();
+
+ProductRouter.post("/product", ProductController.createProduct)
+
+
+module.exports = ProductRouter;
